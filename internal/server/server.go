bc7d7968c5b4d26c3540df951d20f78ff547ca66
Авдеев Матвей Анатольевич <maavdeev@edu.hse.ru>
2026-03-11 11:56:54 +0300

    feature: server struct is created

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,45 @@
 package server
 
+import (
+	"context"
+	"homework/config"
+	"homework/internal/services/order"
+	"homework/pkg/closer"
+	"homework/pkg/load_config"
+	"homework/pkg/logger"
+	"log"
+	"log/slog"
+)
+
 type Server struct {
-	closer
+	logger *slog.Logger
+	closer closer.Closer
+	ctx    context.Context
+	config config.Config
+
+	service order.OrderServiceServer
+}
+
+//"./config/.env"
+func NewServer(configPath string) *Server { //TODO: добавить closer
+
+	ctx := context.Background()
+	err := load_config.LoadDotEnv(configPath)
+	if err != nil {
+		log.Fatalf("server.NewServer: failed to load .env file: %v", err)
+	}
+
+	appConfig := config.NewConfig()
+	logger.Setup(appConfig.EnvType())
+
+	lg := logger.With("service_name", "order-service")
+
+	orderServiceServer := order.NewOrderServiceServer()
+
+	return &Server{
+		logger:  lg,
+		ctx:     ctx,
+		config:  *appConfig,
+		service: *orderServiceServer,
+	}
 }
